9e6fe12e50604ae8f2c561c99e34404d6337b74e
saasbook <saasbook@saasbook.(none)>
2012-11-08 15:54:31 +0800

    hw4-add director to movie complete

--- Gemfile
+++ Gemfile
@@ -6,17 +6,21 @@ gem 'rails', '3.1.0'
 # gem 'rails',     :git => 'git://github.com/rails/rails.git'
 
 # for Heroku deployment - as described in Ap. A of ELLS book
-group :development, :test do
+group :development do
   gem 'sqlite3'
   gem 'ruby-debug19', :require => 'ruby-debug'
-  gem 'cucumber-rails'
-  gem 'cucumber-rails-training-wheels'
   gem 'database_cleaner'
   gem 'capybara'
   gem 'launchy'
   gem 'rspec-rails'
   gem 'simplecov'
 end
+
+group :test do
+  gem 'cucumber-rails'
+  gem 'cucumber-rails-training-wheels'
+end
+
 group :production do
   gem 'pg'
 end
---
+++ features/step_definitions/movie_steps.rb
@@ -0,0 +1,81 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
+  end
+#flunk "Unimplemented"
+end
+
+Given /I am on the details page for "(.*)"/ do |title|
+  movie = Movie.find_by_title("#{title}")
+  visit %(/movies/#{movie[:id]})
+end
+
+When /I go to the edit page for "(.*)"/ do |movie_title|
+  movie = Movie.find_by_title("#{movie_title}")
+#step %Q(go to /movies/#{movie[:id]}/edit)
+#  step %Q(I go to the edit page for #{movie[:id]})
+  visit %Q(/movies/#{movie[:id]}/edit)
+end
+
+Then /the director of "(.*)" should be "(.*)"/ do |title, director|
+  movie = Movie.find_by_title("#{title}")
+  if (movie[:director] != director) then flunk "the director should be #{director}" end
+end
+
+Then /I should see the following movies/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    if !(movie[:rating] =~ /[PG||R]/) then flunk "sort by rating is not working" end
+  end
+#flunk "Unimplemented"
+end
+
+Then /I should not see the following movies/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    if !(movie[:rating] =~ /[PG\-13||G]/) then flunk "sort by rating is not working" end
+  end
+#flunk "Unimplemented"
+end
+
+Then /I should not see any movies/ do
+  rows= page.all("table#movies tbody tr td[1]").map {|t| t.text}.size
+  rows.should == 0
+end
+
+Then /I should see all the movies/ do
+  rows= page.all("table#movies tbody tr td[1]").map {|t| t.text}.size
+  rows.should == Movie.all.size
+end
+
+
+# Make sure that one string (regexp) occurs before or after another one
+#   on the same page
+
+Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
+  #  ensure that that e1 occurs before e2.
+  #  page.body is the entire content of the page as a string.
+  puts page.body.include? "/(.*)#{e1}(.*)#{e2}/"
+end
+
+# Make it easier to express checking or unchecking several boxes at once
+#  "When I uncheck the following ratings: PG, G, R"
+#  "When I check the following ratings: G"
+
+When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+  # HINT: use String#split to split up the rating_list, then
+  #   iterate over the ratings and reuse the "When I check..." or
+  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/,\s*/).each do |rate|
+    if uncheck == "un"
+      step %Q(I uncheck #{rate})
+    else
+      step %Q(I check #{rate})
+    end
+  end
+end
+
